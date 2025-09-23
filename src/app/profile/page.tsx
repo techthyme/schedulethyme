2e581ccf45a3a3cc4e6a8f5d079477aa308582d1
Antonio Rosario <asaptonic@Antonios-MacBook-Air.local>
2025-09-23 19:30:05 -0400

    page.tsx renders

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,5 +2,10 @@ import SVR_Profile from "@/components/server/profiles";
 import ProfileComponent from "@/components/client/profiles";
 
 export default function Profile() {
-  return <SVR_Profile />;
+  return(
+<div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+      <h1 className="text-3xl font-bold mb-4">My Profile</h1>
+      <p className="text-gray-700">Hello.</p>
+    </div>
+  );
 }
